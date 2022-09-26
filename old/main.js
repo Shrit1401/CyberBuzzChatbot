49678d00beefdf82473f82d493a0360123b005f5
Shrit1401 <shrit1401@gmail.com>
2022-09-26 18:26:20 +0530

    first commit

---
+++ old/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $(".btn").on("click", function () {
+        buttonClicked();
+    });
+
+    $("#data").addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            $(".btn").click();
+        }   
+    });
+});
+
+function buttonClicked() {
+    $value = $("#data").val();
+    $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>' + $value + '</p></div></div>';
+    $(".form").append($msg);
+    $("#data").val('');
+
+    // start ajax code
+    $.ajax({
+        url: 'message.php',
+        type: 'POST',
+        data: 'text=' + $value,
+        success: function (result) {
+            $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>' + result + '</p></div></div>';
+            $(".form").append($replay);
+            // when chat goes down the scroll bar automatically comes to the bottom
+            $(".form").scrollTop($(".form")[0].scrollHeight);
+        }
+    });
+}
+
+
