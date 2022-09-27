cc640f4f075e19a7b6f4d94bcba902f9ba9ceead
Shrit1401 <shrit1401@gmail.com>
2022-09-27 16:30:21 +0530

    Responsive-2

---
+++ ui.js
@@ -0,0 +1,12 @@
+// kebab(three dots)
+
+var kebab = document.querySelector('.kebab'),
+    middle = document.querySelector('.middle'),
+    cross = document.querySelector('.cross'),
+    dropdown = document.querySelector('.dropdown');
+
+kebab.addEventListener('click', function () {
+    middle.classList.toggle('active');
+    cross.classList.toggle('active');
+    dropdown.classList.toggle('active');
+})
